304022b8519e9e72877ab5d5a78665383b1917b1
kabsmeiou <cvincentcabral@gmail.com>
2024-09-11 13:22:32 +0800

    quick fix entry element

--- frontend/src/components/Entry.jsx
+++ frontend/src/components/Entry.jsx
@@ -12,7 +12,7 @@ function Entry({note, setTitle, setContent, setReadOnly, setCurrentNoteId, setSh
 
   return (
     <div className="flex gap-x-4 w-full hover:bg-[#D4A373] rounded" onClick={togglePaper}>
-      <div className='h-[60px] w-[2px] bg-[#D4A373]'></div>
+      <div className='h-full w-[2px] bg-[#D4A373]'></div>
       <div className='flex flex-col justify-center w-full'>
         <div className='w-full lg:text-[22px] md:text-[16px]'>{note.title}</div>
         <div className='w-full lg:text-[16px] md:text-[10px]'>{note.get_time_and_date}</div>
